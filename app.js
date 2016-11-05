1619bc83791a3e4492121795160ad80b8b0a8900
techntv <techntvz@gmail.com>
2016-11-05 09:35:07 +0700

    Make random color with rgba

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
       var r = getRandomInt(0,255);
       var g = getRandomInt(0,255);
       var b = getRandomInt(0,255);
+      var a = Math.random().toFixed(1);      
 
-        $('body').css('backgroundColor','rgb(' + r + ',' + g + ',' + b + ')');
+        $('body').css('backgroundColor','rgba(' + r + ',' + g + ',' + b + ',' + a + ')');
     })
 });
